fe8ac18bd6884f20fb361a236440120d6f1ea6e5
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2026-08-11 22:30:16 -0300

    feat(model): adiciona modelos padronizados de resposta
    - adiciona o PagedResponse para representar respostas paginadas
    - inclui informações de paginação como página, tamanho e total de itens
    - adiciona suporte à verificação de próxima e página anterior
    - adiciona o ResponseModel para padronizar respostas da API
    - inclui dados, mensagem e status nas respostas
    - prepara a aplicação para retornos padronizados e paginados

---
+++ LinqEfCoreLab/Common/ResponseModel.cs
@@ -0,0 +1,8 @@
+namespace LinqEfCoreLab.Common;
+
+public class ResponseModel<T>
+{
+    public T? Dados { get; set; }
+    public string Mensagem { get; set; } = string.Empty;
+    public bool Status { get; set; } = true;
+}
